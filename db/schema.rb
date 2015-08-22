dbc164aecdcb49e1d4e647d55bf68f62d09a7afb
Angela Mancini <angela.mancini@sage.com>
2015-08-22 18:02:16 -0400

    switch db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150822213225) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "group_traits", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "trait_id"
