4ab8022f7cbc86a015969f9e554ea48821bdec86
Angela Mancini <angela.mancini@sage.com>
2015-08-23 17:54:30 -0400

    Add support for different template types

--- app/helpers/templates_helper.rb
+++ app/helpers/templates_helper.rb
@@ -1,10 +1,40 @@
 module TemplatesHelper
 
-  def write_dots(start_value, max_value)
+  def show_trait(trait, trait_type)
+    case trait_type
+    when "general"
+      "<strong>#{trait.name}</strong> <span class='pull-right'>_____________________</span>".html_safe
+    when "dots"
+      "<strong>#{trait.name}</strong> #{write_dots(trait.start_value, trait.max_value, 1)}".html_safe
+    when "bigdots"
+      "<strong>#{trait.name}</strong><br />#{write_dots(trait.start_value, trait.max_value, 2)}".html_safe
+    when "boxedbigdots"
+      big_dots= "<i class='fa fa-circle-o fa-fw'></i> " * trait.max_value.to_i
+      boxes = "<i class='fa fa-square-o fa-fw'></i> " * trait.max_value.to_i
+      "<strong>#{trait.name}</strong><br />#{big_dots}<br />#{boxes}".html_safe
+    when "slots"
+      "_____________________"
+    when "doubleslots"
+      "_____________________ <span class='pull-right'>_____________________</span>".html_safe
+    when "merits"
+      "#{trait.name} <span class='pull-right'>_________</span>".html_safe
+    when "health"
+      "#{trait.name} <span class='pull-right'>_________ <i class='fa fa-square-o'></i></span>".html_safe
+    when "circle"
+      line = "<i class='fa fa-square-o'></i></span> " * 10
+      "<strong>#{trait.name}</strong><br />#{line}<br />#{line}".html_safe
+    else
+      trait_type
+    end
+
+  end
+
+  def write_dots(start_value, max_value, size)
+    modifier = "fa-#{size}x"
     start_value = start_value.to_i
     max_value = max_value.to_i
-    starting_dots = "<i class='fa fa-circle'></i> " * start_value
-    empty_dots = "<i class='fa fa-circle-o'></i> " * (max_value - start_value)
+    starting_dots = "<i class='fa fa-circle #{modifier}'></i> " * start_value
+    empty_dots = "<i class='fa fa-circle-o #{modifier}'></i> " * (max_value - start_value)
     return "<span class='pull-right'>#{starting_dots}#{empty_dots}</span>".html_safe
   end
 end
--- lib/modules/parser/characturge.rb
+++ lib/modules/parser/characturge.rb
@@ -4,13 +4,43 @@ module Parser
     require 'json'
     class << self
 
-      def segments(template)
+      def segments(segments_hash)
       end
 
       def groups(template)
       end
 
-      def traits(template)
+      def get_traits(traits, values, value_lists, max_values, start_values, freebie_cost, experiance_dot_cost, experiance_new_cost)
+        trait_array = []
+        traits.each_with_index do |trait, index|
+          trait_details = {}
+          trait_details[:trait] = trait
+          if max_values
+            if max_values.count == 1
+              trait_details[:max_value] = max_values.first
+            else
+              trait_details[:max_value] = max_values[index]
+            end
+          end
+          if start_values
+            if start_values.count == 1
+              trait_details[:start_value] = start_values.first
+            else
+              trait_details[:start_value] = start_values[index]
+            end
+          end
+          if freebie_cost
+            trait_details[:freebie_cost] = freebie_cost[index]
+          end
+          if experiance_dot_cost
+            trait_details[:experiance_dot_cost] = experiance_dot_cost[index]
+          end
+          if experiance_new_cost
+            trait_details[:experiance_new_cost] = experiance_new_cost[index]
+          end
+          trait_array.push(trait_details)
+        end
+        return trait_array
       end
 
       def parse_gct(file)
@@ -42,34 +72,16 @@ module Parser
                 show_heading = template[sg]['ShowHeading']
                 dots_available = template[sg]['DotsAvailable']
                 type = template[sg]['Type'].downcase
+                # Traits!
                 traits = template[sg]['Traits'].split(',')
-                values = template[sg]['Values'].split(',') .split(',') unless template[sg]['Values'].nil?
+                values = template[sg]['Values'].split(',') unless template[sg]['Values'].nil?
+                value_lists = template[sg]['ValueLists'].split(',') unless template[sg]['ValueLists'].nil?
                 max_values = template[sg]['MaxValues'].split(',') unless template[sg]['MaxValues'].nil?
                 start_values = template[sg]['StartValues'].split(',') unless template[sg]['StartValues'].nil?
                 freebie_cost = template[sg]['FreebieCost'].split(',') unless template[sg]['FreebieCost'].nil?
                 experiance_dot_cost = template[sg]['ExperienceDotCost'].split(',') unless template[sg]['ExperienceDotCost'].nil?
                 experiance_new_cost = template[sg]['ExperienceNewCost'].split(',') unless template[sg]['ExperienceNewCost'].nil?
-                trait_array = []
-                traits.each_with_index do |trait, index|
-                  trait_details = {}
-                  trait_details[:trait] = trait
-                  if max_values
-                    trait_details[:max_value] = max_values[index]
-                  end
-                  if start_values
-                    trait_details[:start_value] = start_values[index]
-                  end
-                  if freebie_cost
-                    trait_details[:freebie_cost] = freebie_cost[index]
-                  end
-                  if experiance_dot_cost
-                    trait_details[:experiance_dot_cost] = experiance_dot_cost[index]
-                  end
-                  if experiance_new_cost
-                    trait_details[:experiance_new_cost] = experiance_new_cost[index]
-                  end
-                  trait_array.push(trait_details)
-                end
+                trait_array = get_traits(traits, values, value_lists, max_values, start_values, freebie_cost, experiance_dot_cost, experiance_new_cost)
 
                 # trait_values
                 segment[:groups].push({ group: sg, number: number, show_heading: show_heading, dots_available: dots_available, type: type, traits: trait_array })
